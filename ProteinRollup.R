daf5cd782e34bc3fd95190f4f8f2d8ac2ef4cdf5
Jakob <jakob.willforss@hotmail.com>
2019-09-27 16:54:55 +0200

    Incorrectly functioning presence filtering now running correctly

--- ProteinRollup.R
+++ ProteinRollup.R
@@ -1,6 +1,6 @@
 library(outliers)
-suppressPackageStartupMessages(library(tidyverse))
-library(matrixStats)
+suppressPackageStartupMessages(library(tidyverse, warn.conflicts=FALSE))
+library(matrixStats, warn.conflicts=FALSE)
 
 # Inspired by DANTE and pmartR
 # https://github.com/PNNL-Comp-Mass-Spec/InfernoRDN/blob/master/Rscripts/Rollup/RRollup.R
@@ -74,7 +74,11 @@ protein_rollup = function(protein_ids, pep_mat, rollup_func=rrollup, protein_col
    
 passes_presence_threshold <- function(peptides, presence_fraction) {
     
-    max(rowSums(!is.na(peptides))) > (ncol(peptides) * presence_fraction)
+    if (nrow(peptides) > 1) {
+        message(paste(peptides, collapse=", "), " ", max(rowSums(!is.na(peptides))) / ncol(peptides))
+    }
+    
+    max(rowSums(!is.na(peptides))) / ncol(peptides) >= presence_fraction
 }
  
 rrollup = function(peptides, combine_func=median, min_overlap=3, get_debug_info=FALSE, minPs=5, outlier_pval=0.05) {
@@ -261,44 +265,61 @@ main <- function() {
     # source(argv$protein_rollup_path)
     raw_rdf <- read_tsv(argv$rdf_fp, col_types=cols())
     
+    if (argv$min_presence < 0 || argv$min_presence > 1) {
+        stop("--min_presence expected to be in range 0 to 1, found: ", argv$min_presence)
+    }
+    
     if (!argv$one_column_mode) {
         ddf <- read_tsv(argv$ddf_fp, col_types=cols())
-        rdf <- raw_rdf %>% filter(!is.na(UQ(as.name(argv$protein_col))))
+        rdf <- raw_rdf
         sdf <- rdf %>% dplyr::select(dplyr::one_of(ddf[[argv$sample_col]])) %>% as.matrix()
         protein_data <- rdf %>% dplyr::select(argv$protein_col) %>% unlist()
+        protein_data[is.na(protein_data)] <- "NA"
     }
     else {
         sdf <- raw_rdf[, -1] %>% as.matrix()
         protein_data <- raw_rdf[, 1] %>% unlist()
+        protein_data[is.na(protein_data)] <- "NA"
     }
     
     message("Performing rollup for ", nrow(raw_rdf), " peptides for ", length(unique(protein_data)), " unique protein IDs")
     
-    prot_sdf <- protein_rollup(protein_data, as.matrix(sdf), one_hit_wonders=argv$one_hit_wonders, min_presence=argv$min_presence, min_overlap=argv$min_overlap)
-    write_tsv(prot_sdf, path=argv$out_fp)
+    prot_adf <- protein_rollup(
+        protein_data,
+        as.matrix(sdf),
+        one_hit_wonders=argv$one_hit_wonders,
+        min_presence=argv$min_presence,
+        min_overlap=argv$min_overlap
+    )
+
+    if (argv$out_protein_name != "") {
+        prot_adf <- cbind(prot_adf[["Protein"]], prot_adf)
+        colnames(prot_adf) <- c(argv$out_protein_name, colnames(prot_adf)[-1])
+    }
+
+    write_tsv(prot_adf, path=argv$out_fp)
     message("Resulting file written to ", argv$out_fp)
 }
 
 parse_input_params <- function() {
-    parser <- arg_parser("Protein rollup, standalone wrapper")
-    parser <- add_argument(parser, "--ddf_fp", help="Design matrix path", type="character", default=NA)
-    parser <- add_argument(parser, "--rdf_fp", help="Raw matrix path", type="character", nargs=1)
-    parser <- add_argument(parser, "--out_fp", help="Output matrix path", type="character", nargs=1)
-    parser <- add_argument(parser, "--sample_col", help="Design matrix sample column", type="character", default=NA)
-    parser <- add_argument(parser, "--protein_col", help="Protein column in main data frame", type="character", default=NA)
+    parser <- argparser::arg_parser("Protein rollup, standalone wrapper")
+    parser <- argparser::add_argument(parser, "--ddf_fp", help="Design matrix path", type="character", default=NA)
+    parser <- argparser::add_argument(parser, "--rdf_fp", help="Raw matrix path", type="character", nargs=1)
+    parser <- argparser::add_argument(parser, "--out_fp", help="Output matrix path", type="character", nargs=1)
+    parser <- argparser::add_argument(parser, "--sample_col", help="Design matrix sample column", type="character", default=NA)
+    parser <- argparser::add_argument(parser, "--protein_col", help="Protein column in main data frame", type="character", default=NA)
     
-    parser <- add_argument(parser, "--one_column_mode", help="If first column is the only annotation column, the --ddf_fp, --sample_col and --protein_col argument can be omitted", type="boolean", default=FALSE)
+    parser <- argparser::add_argument(parser, "--one_column_mode", help="If first column is the only annotation column, the --ddf_fp, --sample_col and --protein_col argument can be omitted", type="boolean", default=FALSE)
     
-    
-    parser <- add_argument(parser, "--min_overlap", help="Min. shared overlap between reference peptides and other", type="numeric", default=3)
-    parser <- add_argument(parser, "--one_hit_wonders", help="Should proteins with a single peptide as support be included", type="boolean", default=FALSE)
-    parser <- add_argument(parser, "--min_presence", help="Skip proteins where no peptide is present in at least this percentage", type="numeric", default=0)
-    parser <- add_argument(parser, "--out_protein_name", help="Name of protein column in output", type="character")
+    parser <- argparser::add_argument(parser, "--min_overlap", help="Min. shared overlap between reference peptides and other", type="numeric", default=3)
+    parser <- argparser::add_argument(parser, "--one_hit_wonders", help="Should proteins with a single peptide as support be included", type="boolean", default=FALSE)
+    parser <- argparser::add_argument(parser, "--min_presence", help="Skip proteins where no peptide is present in at least this percentage", type="numeric", default=0)
+    parser <- argparser::add_argument(parser, "--out_protein_name", help="Name of protein column in output", type="character", default="")
     # parser <- add_argument(parser, "--protein_rollup_path", help="CraftOmics protein tools path", type="character", default="ProteinRollup.R")
 
-    parser <- add_argument(parser, "--show_warnings", help="Immediately print warnings", type="bool", default=FALSE)
+    parser <- argparser::add_argument(parser, "--show_warnings", help="Immediately print warnings", type="bool", default=FALSE)
 
-    argv <- parse_args(parser)
+    argv <- argparser::parse_args(parser)
     
     if (!argv$one_column_mode && (is.na(argv$sample_col) || is.na(argv$protein_col) || is.na(argv$ddf_fp))) {
         stop("If not running one_column_mode both --ddf_fp, --sample_col and --protein_col needs to be supplied")
