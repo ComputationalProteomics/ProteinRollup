5f82819f9c7aa3aeae5cf1d9159d0faee82925f2
Jakob <jakob.willforss@hotmail.com>
2019-10-01 12:33:09 +0200

    Possibly solved differences between DanteR and rollup for oat dataset

--- ProteinRollup.R
+++ ProteinRollup.R
@@ -22,12 +22,25 @@ protein_rollup = function(protein_ids, pep_mat, rollup_func=rrollup, protein_col
     
     passing_proteins <- c()
     
+    print_stuff <- FALSE
+    
     for (protein in unique_proteins) {
+        
+        if (protein == "P_3054") {
+            print("FOUND IT")
+            print_stuff <- TRUE
+        }
         
         row_inds <- which(protein_ids == protein)
         current_peps_only <- pep_mat[row_inds, , drop=FALSE]
         
-        if (passes_presence_threshold(current_peps_only, min_presence)) {
+        if (print_stuff) {
+            print(current_peps_only)
+            print("Passing threshold")
+            print(passes_presence_threshold(current_peps_only, min_presence))
+        }
+        
+        if (passes_presence_threshold(current_peps_only, min_presence, print_stuff)) {
             rollup_list <- rollup_func(current_peps_only, get_debug_info=TRUE, min_overlap=min_overlap)
             prot_results[[protein]] <- rollup_list$protein
             scaled_pep_results[[protein]] <- rollup_list$scaled_peptide
@@ -41,9 +54,14 @@ protein_rollup = function(protein_ids, pep_mat, rollup_func=rrollup, protein_col
             #message("Skipping protein ", protein, " due to low presence")
             low_presence_omitted <- low_presence_omitted + 1
         }
+        
+        if (print_stuff) {
+            # stop("Done with PRINT STUFF")
+            print_stuff <- FALSE
+        }
     }
     
-    message(low_presence_omitted, " proteins omitted due to low presence")
+    # message(low_presence_omitted, " proteins omitted due to low presence")
 
     annot_df <- data.frame(Protein=unique_proteins)
     colnames(annot_df) <- c(protein_col_name)
@@ -72,13 +90,33 @@ protein_rollup = function(protein_ids, pep_mat, rollup_func=rrollup, protein_col
     }
 }
    
-passes_presence_threshold <- function(peptides, presence_fraction) {
+passes_presence_threshold <- function(peptides, presence_fraction, print_stuff=FALSE) {
     
-    if (nrow(peptides) > 1) {
-        message(paste(peptides, collapse=", "), " ", max(rowSums(!is.na(peptides))) / ncol(peptides))
+    # if (nrow(peptides) > 1) {
+    #     message(paste(peptides, collapse=", "), " ", max(rowSums(!is.na(peptides))) / ncol(peptides))
+    # }
+    
+    
+    
+    peptides <- peptides[rowSums(!is.na(peptides)) >= 3, , drop=FALSE]
+    
+    if (print_stuff) {
+        print(rowSums(!is.na(peptides)))
+        print(rowSums(!is.na(peptides)) >= 3)
+        print(peptides[rowSums(!is.na(peptides)) >= 3, , drop=FALSE])
+        print("Best presence")
+        print(max(rowSums(!is.na(peptides))) / ncol(peptides))
+        print("Rowsums:")
+        print(rowSums(!is.na(peptides)))
+        print("New setup")
+        print(length(which(colSums(!is.na(peptides)) > 0)) / ncol(peptides))
     }
     
-    max(rowSums(!is.na(peptides))) / ncol(peptides) >= presence_fraction
+    # Any peptide should match the coverage
+    length(which(colSums(!is.na(peptides)) > 0)) / ncol(peptides) >= presence_fraction
+    
+    # Best peptide should match minimum coverage
+    # max(rowSums(!is.na(peptides))) / ncol(peptides) >= presence_fraction
 }
  
 rrollup = function(peptides, combine_func=median, min_overlap=3, get_debug_info=FALSE, minPs=5, outlier_pval=0.05) {
@@ -297,8 +335,8 @@ main <- function() {
         colnames(prot_adf) <- c(argv$out_protein_name, colnames(prot_adf)[-1])
     }
 
+    message(sprintf("Writing %s proteins to %s", nrow(prot_adf), argv$out_fp))
     write_tsv(prot_adf, path=argv$out_fp)
-    message("Resulting file written to ", argv$out_fp)
 }
 
 parse_input_params <- function() {
@@ -312,6 +350,8 @@ parse_input_params <- function() {
     parser <- argparser::add_argument(parser, "--one_column_mode", help="If first column is the only annotation column, the --ddf_fp, --sample_col and --protein_col argument can be omitted", type="boolean", default=FALSE)
     
     parser <- argparser::add_argument(parser, "--min_overlap", help="Min. shared overlap between reference peptides and other", type="numeric", default=3)
+    # parser <- argparser::add_argument(parser, "--peptide_min_presence_count", help="Exclude peptides present in a lower count than this", type="numeric", default=3)
+    
     parser <- argparser::add_argument(parser, "--one_hit_wonders", help="Should proteins with a single peptide as support be included", type="boolean", default=FALSE)
     parser <- argparser::add_argument(parser, "--min_presence", help="Skip proteins where no peptide is present in at least this percentage", type="numeric", default=0)
     parser <- argparser::add_argument(parser, "--out_protein_name", help="Name of protein column in output", type="character", default="")
